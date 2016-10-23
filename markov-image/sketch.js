780cd9f8e75ed562e5d79e116d45287d23491448
Yiting Liu <lyiting0901@gmail.com>
2016-10-23 04:43:04 -0400

    markov-image

--- markov-image/sketch.js
+++ markov-image/sketch.js
@@ -1,37 +1,56 @@
-var img1,img2;
-var imageSize=50;
-var colors={};
+var img;
+var imageSize = 100;
+var colors = {};
 
 // var d=pixelDensity;
-function preload(){
-	img1=loadImage("img/starry.png");
-	// img2=loadImage("img/sunflower.png");
+function preload() {
+  img = loadImage("img/starry.png");
 }
 
 
 function setup() {
-	createCanvas(1500,500);
-	image(img1,0,0,imageSize,imageSize);
-	// image(img2,imageSize,0,imageSize,imageSize);
-	getPixels(img1);
-	console.log(colors);
+  createCanvas(imageSize, imageSize*2);
+  image(img, 0, 0, imageSize, imageSize);
+  getPixels();
+  // console.log(colors);
+  startColor=get(floor(random(imageSize)),floor(random(imageSize)));
+  console.log(startColor);
+  setPixels(startColor);
 }
 
-function getPixels(img){
-	img.loadPixels();
-	for(var i=0;i<img.width;i++){
-		for(var j=0;j<img.height;j++){
-			var c=img.get(i,j);
-			if(!colors[c]){
-				colors[c]=[];
-			}
-			if(j>=img.heigh){
-				colors[c].push(img.get(i+1,0));
-			}else{
-				colors[c].push(img.get(i,j+1))
-			}
-		}
-	}
+function getPixels() {
+  for (var i = 0; i < imageSize; i++) {
+    for (var j = 0; j < imageSize - 1; j++) {
+      var c = get(i, j);
+      // if (!colors[c]) {
+      //   colors[c] = 1;
+      // } else{
+      //   colors[c] += 1;
+      //   console.log(c+":"+colors[c]);
+      // }
+
+      if (!colors[c]) {
+        colors[c] = [];
+      }
+      colors[c].push(get(i, j + 1));
+    }
+  }
 }
 
-
+function setPixels(next){
+  console.log("startSetting");
+  for (var i = 0; i < imageSize; i++) {
+    for (var j = imageSize; j < imageSize+imageSize; j++) {
+      if(i==0 && j==0){
+        set(i,j,next);
+      } else {
+        currentColor=get(i,j-imageSize);
+        next=random(colors[currentColor]);
+        console.log(next);
+        set(i,j,next);
+      }
+    }
+  }
+  updatePixels();
+  console.log("completed!")
+}
