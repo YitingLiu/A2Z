2bc7e4824c1a44ad1fc205bee34573385584456f
Yiting Liu <lyiting0901@gmail.com>
2016-10-24 01:13:04 -0400

    markov

--- markov-image/sketch.js
+++ markov-image/sketch.js
@@ -1,19 +1,24 @@
 var img;
 var imageSize = 100;
 var colors = {};
+var offset = 30+imageSize;
 
 // var d=pixelDensity;
 function preload() {
-  img = loadImage("img/starry.png");
+  img = loadImage("img/sunflower.png");
 }
 
 
 function setup() {
-  createCanvas(imageSize, imageSize*2);
+  createCanvas(imageSize, imageSize * 2 + offset);
   image(img, 0, 0, imageSize, imageSize);
   getPixels();
   // console.log(colors);
-  startColor=get(floor(random(imageSize)),floor(random(imageSize)));
+
+  var startX = floor(random(imageSize));
+  var startY = floor(random(imageSize));
+  console.log(startX + " " + startY);
+  startColor = get(startX, startY);
   console.log(startColor);
   setPixels(startColor);
 }
@@ -37,17 +42,29 @@ function getPixels() {
   }
 }
 
-function setPixels(next){
-  console.log("startSetting");
+function setPixels(startColor) {
+  // console.log("startSetting");
+
+  var currentColor = startColor;
+
+
   for (var i = 0; i < imageSize; i++) {
-    for (var j = imageSize; j < imageSize+imageSize; j++) {
-      if(i==0 && j==0){
-        set(i,j,next);
+    for (var j = 0; j < imageSize; j++) {
+      if (i == 0 && j == 0) {
+        set(i, j + offset, currentColor);
       } else {
-        currentColor=get(i,j-imageSize);
-        next=random(colors[currentColor]);
-        console.log(next);
-        set(i,j,next);
+        var possibilities = colors[currentColor];
+        if (!possibilities) {
+          console.log("random");
+          //get a new color:
+          var x = floor(random(imageSize));
+          var y = floor(random(imageSize));
+          var next = get(x, y);
+        } else {
+          var next = random(possibilities);
+        }
+        set(i, j + offset, next);
+        currentColor = next;
       }
     }
   }
